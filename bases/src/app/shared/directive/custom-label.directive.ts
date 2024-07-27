4ad5a718ea10445b6dd67d779ed78df27f14dc31
German Andres Ortiz Martinez <germ.ortiz@udla.edu.co>
2024-07-27 15:18:45 -0500

    Agregando los mensajes de error a la etiqueta del span indicando que le falta al usuario

--- bases/src/app/shared/directive/custom-label.directive.ts
+++ bases/src/app/shared/directive/custom-label.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
 
 @Directive({
   selector: '[customLabel]'
@@ -7,11 +8,17 @@ export class CustomLabelDirective implements OnInit{
 
   private htmlElement?: ElementRef<HTMLElement>;
   private _color: string = 'red';
+  private _errors?: ValidationErrors | null;
 
   // set permite cambiar el valor de la directiva
   @Input() set color(value: string){
     this._color = value;
     this.setStyle();
+  }
+
+  @Input() set errors(value: ValidationErrors | null | undefined){
+    this._errors = value;
+    this.setErrorMessage();
   }
 
   constructor( private el: ElementRef<HTMLElement>) {
@@ -29,4 +36,35 @@ export class CustomLabelDirective implements OnInit{
     this.htmlElement!.nativeElement.style.color = this._color;
   }
 
+  setErrorMessage(): void {
+    if( !this.htmlElement ) return;
+    if( !this._errors) {
+      this.setText('');
+      return;
+    }
+
+    const errors = Object.keys(this._errors);
+
+    if( errors.includes('required')){
+      this.setText('Este campo es requerido.');
+      return;
+    }
+
+    if( errors.includes('minlength')){
+      const min = this._errors['minlength']['requiredLength'];
+      const current = this._errors['minlength']['actualLength'];
+      this.setText(`Mínimo ${current}/${min} caracteres.`);
+      return;
+    }
+
+    if( errors.includes('email')){
+      this.setText('No tiene formato de correo.');
+      return;
+    }
+
+  }
+
+  setText(value: string){
+    this.htmlElement!.nativeElement.innerText = value;
+  }
 }
